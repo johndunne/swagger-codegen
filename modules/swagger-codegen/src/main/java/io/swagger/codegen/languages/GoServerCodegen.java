d42a5d4f417d75ff257ec57da1418f7116815e9e
John Dunne <john.dunne@epexcel.com>
2016-09-21 17:40:58 +0100

    Support for custom DB and oauth setup

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/GoServerCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/GoServerCodegen.java
@@ -1,27 +1,14 @@
 package io.swagger.codegen.languages;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Multimap;
 import io.swagger.codegen.*;
-import io.swagger.models.*;
 import io.swagger.models.properties.ArrayProperty;
 import io.swagger.models.properties.MapProperty;
 import io.swagger.models.properties.Property;
-import io.swagger.util.Yaml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.*;
-import java.util.Map.Entry;
 import org.apache.commons.lang3.StringUtils;
 
 public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
@@ -31,24 +18,51 @@ public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
     protected String apiVersion = "1.0.0";
     protected int serverPort = 8080;
     protected String projectName = "swagger-server";
-    protected String apiPath = "go";
-    
+    protected String apiPath = "src/quickflickserver/controllers";
+    protected String projectRoot = "src/quickflickserver";
+
+    protected String PROJECT_NAME = "lah";
+    protected String DB_DIALECT = "db_dialect";
+    protected String dbDialect = "mysql";
+    protected String DB_BACKEND = "db_backend";
+    protected String dbBackend = "gorm";
+    protected String DB_USER = "db_user";
+    protected String dbUser = "user";
+    protected String DB_PASSWORD = "db_password";
+    protected String dbPassword = "password";
+    protected String DB_NAME = "db_name";
+    protected String dbName = "dbname";
+    protected String USE_OAUTH = "use_oauth";
+    protected boolean useOauth = true;
+    protected String OAUTH_SECRET_KEY = "oauth_secret_key";
+    protected String oauthSecretKey = "SECRETNESS_PLEASE";
+    protected String LISTEN_PORT = "listen_port";
+    protected int listenPort = 8961;
+    protected String INCLUDE_PLACEHOLDER_HANDLERS = "include_placeholder_handlers";
+    protected boolean includePlaceHolderHandlers = false;
+    protected String HANDLERS_PACKAGE_NAME = "handlers_package_name";
+    protected String handlersPackageName = "handlers";
+
+
     public GoServerCodegen() {
         super();
 
         // set the output folder here
         outputFolder = "generated-code/go";
 
-        /**
+        /*
          * Models.  You can write model files using the modelTemplateFiles map.
          * if you want to create one template for file, you can do so here.
          * for multiple files for model, just put another entry in the `modelTemplateFiles` with
          * a different extension
          */
+        modelPackage = "src/quickflickserver/models";
+        apiPackage = "src/quickflickserver/controllers";
+
         modelTemplateFiles.clear();
         modelTemplateFiles.put("models.mustache", ".go");
 
-        /**
+        /*
          * Api classes.  You can write classes for each Api file with the apiTemplateFiles map.
          * as with models, add multiple entries with different extensions for multiple files per
          * class
@@ -57,13 +71,13 @@ public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
                 "controller.mustache",   // the template to use
                 ".go");       // the extension for each file to write
 
-        /**
+        /*
          * Template Location.  This is the location which templates will be read from.  The generator
          * will use the resource stream to attempt to read the templates.
          */
         embeddedTemplateDir = templateDir = "go-server";
 
-        /**
+        /*
          * Reserved words.  Override this with reserved words specific to your language
          */
         setReservedWordsLowerCase(
@@ -130,6 +144,13 @@ public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
         cliOptions.clear();
         cliOptions.add(new CliOption(CodegenConstants.PACKAGE_NAME, "Go package name (convention: lowercase).")
                 .defaultValue("swagger"));
+        cliOptions.add(new CliOption(PROJECT_NAME, "Project name in Xcode"));
+        cliOptions.add(new CliOption(DB_BACKEND, "Which db should be used for the backend. gorm is currently available."));
+        cliOptions.add(new CliOption(DB_USER, "The database username."));
+        cliOptions.add(new CliOption(DB_PASSWORD, "The database password."));
+        cliOptions.add(new CliOption(DB_NAME, "The database name to connect to."));
+        cliOptions.add(new CliOption(USE_OAUTH, "Use oauth.com for authorisation and authentcaiton?"));
+        cliOptions.add(new CliOption(OAUTH_SECRET_KEY, "OAuth secret key"));
         /**
          * Additional Properties.  These values can be passed to the templates and
          * are available in models, apis, and supporting files
@@ -137,7 +158,7 @@ public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
         additionalProperties.put("apiVersion", apiVersion);
         additionalProperties.put("serverPort", serverPort);
         additionalProperties.put("apiPath", apiPath);
-        /**
+        /*
          * Supporting Files.  You can write single files for the generator with the
          * entire object tree available.  If the input file has a suffix of `.mustache
          * it will be processed by the template engine.  Otherwise, it will be copied
@@ -146,12 +167,13 @@ public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
                         "api",
                         "swagger.yaml")
         );
-        supportingFiles.add(new SupportingFile("main.mustache", "", "main.go"));
-        supportingFiles.add(new SupportingFile("routers.mustache", apiPath, "routers.go"));
-        supportingFiles.add(new SupportingFile("logger.mustache", apiPath, "logger.go")); 
-        supportingFiles.add(new SupportingFile("app.mustache", apiPath, "app.yaml"));        
-        supportingFiles.add(new SupportingFile("json_object.mustache", apiPath, "json_operations.go"));
-        writeOptional(outputFolder, new SupportingFile("README.mustache", apiPath, "README.md"));
+        //supportingFiles.add(new SupportingFile("main.mustache", "src/quickflickservermain", "main.go"));
+        supportingFiles.add(new SupportingFile("routers.mustache", projectRoot, "routers.go"));
+        supportingFiles.add(new SupportingFile("gorm_models.mustache", projectRoot, "gorm_models.go"));
+        supportingFiles.add(new SupportingFile("logger.mustache", projectRoot, "logger.go"));
+        supportingFiles.add(new SupportingFile("app.mustache", projectRoot, "app.yaml"));
+        supportingFiles.add(new SupportingFile("json_object.mustache", projectRoot+"/helpers", "json_operations.go"));
+        writeOptional(outputFolder, new SupportingFile("README.mustache", projectRoot, "README.md"));
     }
 
     @Override
@@ -325,6 +347,109 @@ public class GoServerCodegen extends DefaultCodegen implements CodegenConfig {
     @Override
     public String escapeUnsafeCharacters(String input) {
         return input.replace("*/", "*_/").replace("/*", "/_*");
+    }
+
+    public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> objectMap = (Map<String, Object>) objs.get("operations");
+        @SuppressWarnings("unchecked")
+        List<CodegenOperation> operations = (List<CodegenOperation>) objectMap.get("operation");
+        for (CodegenOperation operation : operations) {
+            // http method verb conversion (e.g. PUT => Put)
+            // http method verb conversion (e.g. PUT => Put)
+            operation.httpMethod = camelize(operation.httpMethod.toLowerCase());
+        }
+
+        // remove model imports to avoid error
+        List<Map<String, String>> imports = (List<Map<String, String>>) objs.get("imports");
+        if (imports == null)
+            return objs;
+
+        Iterator<Map<String, String>> iterator = imports.iterator();
+        while (iterator.hasNext()) {
+            String _import = iterator.next().get("import");
+            if (_import.startsWith(apiPackage()))
+                iterator.remove();
+        }
+        // if the return type is not primitive, import encoding/json
+        for (CodegenOperation operation : operations) {
+            if(operation.returnBaseType != null && needToImport(operation.returnBaseType)) {
+                imports.add(createMapping("import", "encoding/json"));
+                break; //just need to import once
+            }
+        }
+
+        // this will only import "strings" "fmt" if there are items in pathParams
+        for (CodegenOperation operation : operations) {
+            if(operation.pathParams != null && operation.pathParams.size() > 0) {
+                imports.add(createMapping("import", "fmt"));
+                imports.add(createMapping("import", "strings"));
+                break; //just need to import once
+            }
+        }
+
+
+        // recursively add import for mapping one type to multiple imports
+        List<Map<String, String>> recursiveImports = (List<Map<String, String>>) objs.get("imports");
+        if (recursiveImports == null)
+            return objs;
+
+        ListIterator<Map<String, String>> listIterator = imports.listIterator();
+        while (listIterator.hasNext()) {
+            String _import = listIterator.next().get("import");
+            // if the import package happens to be found in the importMapping (key)
+            // add the corresponding import package to the list
+            if (importMapping.containsKey(_import)) {
+                listIterator.add(createMapping("import", importMapping.get(_import)));
+            }
+        }
+
+        return objs;
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+        if( !additionalProperties.containsKey(DB_BACKEND) ){
+            additionalProperties.put(DB_BACKEND, this.dbBackend);
+        }
+        if( !additionalProperties.containsKey(DB_DIALECT) ){
+            additionalProperties.put(DB_DIALECT, this.dbDialect);
+        }
+        if( !additionalProperties.containsKey(DB_USER) ){
+            additionalProperties.put(DB_USER, this.dbUser);
+        }
+        if( !additionalProperties.containsKey(DB_PASSWORD) ){
+            additionalProperties.put(DB_PASSWORD, this.dbPassword);
+        }
+        if( !additionalProperties.containsKey(DB_NAME) ){
+            additionalProperties.put(DB_NAME, this.dbName);
+        }
+        if( !additionalProperties.containsKey(OAUTH_SECRET_KEY) ){
+            additionalProperties.put(OAUTH_SECRET_KEY, this.oauthSecretKey);
+        }
+        if( !additionalProperties.containsKey(USE_OAUTH) ){
+            additionalProperties.put(USE_OAUTH, this.useOauth);
+        }
+        if( !additionalProperties.containsKey(LISTEN_PORT) ){
+            additionalProperties.put(LISTEN_PORT, this.listenPort);
+        }
+        if( !additionalProperties.containsKey(INCLUDE_PLACEHOLDER_HANDLERS) ){
+            additionalProperties.put(INCLUDE_PLACEHOLDER_HANDLERS, this.includePlaceHolderHandlers);
+        }
+        if( !additionalProperties.containsKey(HANDLERS_PACKAGE_NAME) ){
+            additionalProperties.put(HANDLERS_PACKAGE_NAME, this.handlersPackageName);
+        }
+
+        supportingFiles.add(new SupportingFile("gorm.mustache", projectRoot + "/db", "gorm.go"));
+    }
+
+    public Map<String, String> createMapping(String key, String value){
+        Map<String, String> customImport = new HashMap<String, String>();
+        customImport.put(key, value);
+
+        return customImport;
     }
 
 }
